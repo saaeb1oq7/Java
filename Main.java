323ed4de880f875650df71f58c0bb2144785de39
saaeb1oq7 <133098871+saaeb1oq7@users.noreply.github.com>
2024-10-20 15:53:01 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
 public class Main{
-    public static/* */void main(String[] args) {
-        System.out.println("Hello World");
+    public static void main(String[] args) {
+        System.out.print("Write your grade ");
+        Scanner scn = new Scanner(System.in);
+        int marks = scn.nextInt();
+        if(marks>=90 && marks<=100){
+            System.out.println("Congrats you got an A");
+        }else if(marks>=80 && marks<90){
+            System.out.println("You got a B");
+        }else{
+            System.out.println("You got a C");
+        }
     }
 }
