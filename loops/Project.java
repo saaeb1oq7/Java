f7cc85c7a6a768f9cd94e6a2cd15b514f8d8c845
saaeb1oq7 <133098871+saaeb1oq7@users.noreply.github.com>
2024-11-01 16:31:21 +0300

    Add files via upload

---
+++ loops/Project.java
@@ -0,0 +1,15 @@
+public class Project {
+    public static void main(String[] args) {
+        System.out.println(" \n");
+        int numnew = 1;
+        int numold = 0;
+        int run = 0;        do {
+            run += 1;
+            int numnew2 = numold + numnew;
+            System.out.println(numnew + " + " + numold +" = " + numnew2 );
+            numold = numnew;
+            numnew = numnew2;
+        } while (run < 5);
+        
+    }
+}
