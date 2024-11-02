e40b484b1b86c0406384539a50ce28dcca70fad9
saaeb1oq7 <133098871+saaeb1oq7@users.noreply.github.com>
2024-11-02 15:32:26 +0300

    Update Project.java

--- loops/Project.java
+++ loops/Project.java
@@ -3,7 +3,8 @@ public class Project {
         System.out.println(" \n");
         int numnew = 1;
         int numold = 0;
-        int run = 0;        do {
+        int run = 0;        
+        do {
             run += 1;
             int numnew2 = numold + numnew;
             System.out.println(numnew + " + " + numold +" = " + numnew2 );
