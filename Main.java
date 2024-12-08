7d25e5fa949470b78ef2550118571e56cce69df7
saaeb1oq7 <133098871+saaeb1oq7@users.noreply.github.com>
2024-12-08 15:54:24 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,15 +1,25 @@
-import java.util.Scanner;
+import java.util.*;
+
 public class Main{
     public static void main(String[] args) {
-        System.out.print("Write your grade ");
         Scanner scn = new Scanner(System.in);
-        int marks = scn.nextInt();
-        if(marks>=90 && marks<=100){
-            System.out.println("Congrats you got an A");
-        }else if(marks>=80 && marks<90){
-            System.out.println("You got a B");
-        }else{
-            System.out.println("You got a C");
+        int n = scn.nextInt();
+        int[] arr = new int[n];
+        for (int i=0;i<n;i++){
+            arr[i] = scn.nextInt();
+            
         }
+        int min = arr[0];
+        int max = arr[0];
+
+        for(int i = 1; i<n; i++){
+            if (arr[i] < min){
+                min = arr[i];
+            }
+            if (arr[i] > max){
+                max = arr[i];
+            }
+        }
+        System.out.println("Span of the Array " + max + " - " + min +" = " + (max - min));
     }
 }
--- Main1.java
+++ Main1.java
@@ -1,16 +1,28 @@
-public class Main1{
-    public static void main(String[] args){
-        boolean Rainy = true;
-        boolean cold = false;
-        if(Rainy){
-            if(cold){
-                System.out.println("Get an Umbrella and a jacket");
-            }else{
-                System.out.println("Get an Umbrella");
-            }
-        }else{
-            System.out.println("Go with normal clothes");
+import java.util.*;
+public class Main1 {
+    public static void main(String[] args) {
+        System.out.println("");
+        System.out.println("");
+        Scanner scn = new Scanner(System.in);
+        System.out.println("the Arraylimits");
+        int n = scn.nextInt();
+        System.out.println("enter the Array elements");
+        int[] arr = new int[n];
+        for (int i =0; i<n; i++){
+            arr[i] = scn.nextInt();
         }
-
+        System.out.println("give the number to find");
+        int d = scn.nextInt();
+        int ans = find(arr, d);
+        System.out.println("ans = " + ans);
+        
+    }
+    public static int find(int[] arr, int d) {
+        for (int i = 0; i<arr.length; i++){
+            if (arr[i] == d){
+                return i;
+            }
+        }
+        return -1;
     }
 }
